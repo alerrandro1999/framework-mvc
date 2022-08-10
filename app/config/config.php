5ab7004381e45152f12cb251675c4dac02d83ef5
alerrandro borges <alerrandrokaton@gmail.com>
2022-08-09 23:03:06 -0300

    feat: instalando o twig, e config das vies

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE', 'PHP/mini-framework');
+define('BASE', 'PHP/mini-framework/');
 
 define('UNSET_URI_COUNT', 2);
 define('DEBUG_URI', false);
---
+++ app/view/home/main.twig.php
@@ -0,0 +1,5 @@
+{% extends 'partials/body.twig.php' %}
+{%block title%}Home - Mini Framework{% endblock %}
+{% block body%}
+<h1>teste</h1>
+{% endblock %}
